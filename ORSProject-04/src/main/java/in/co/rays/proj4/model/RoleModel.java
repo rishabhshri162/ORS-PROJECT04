7d1db0d59a86e1d59984f783dbec872cd27d91c5
Rishabh Shrivastava <rishabhshrivastava7565@gmail.com>
2025-11-04 19:45:43 +0530

    Add Role model, exceptions, and JDBC utility

---
+++ ORSProject-04/src/main/java/in/co/rays/proj4/model/RoleModel.java
@@ -0,0 +1,148 @@
+package in.co.rays.proj4.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import in.co.rays.proj4.bean.RoleBean;
+import in.co.rays.proj4.exception.ApplicationException;
+import in.co.rays.proj4.exception.DatabaseException;
+import in.co.rays.proj4.util.JDBCDataSource;
+
+public class RoleModel {
+
+	// next pk method give u next id
+
+	public Integer nextPk() throws DatabaseException {
+		Connection conn = null;
+
+		int pk = 0;
+
+		try {
+
+			conn = JDBCDataSource.getConnection();
+
+			PreparedStatement pstmt = conn.prepareStatement("select max(id) from st_role");
+			ResultSet rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				pk = rs.getInt(1);
+			}
+			rs.close();
+			pstmt.close();
+
+		} catch (Exception e) {
+			throw new DatabaseException("Exception: Exception in getting pk");
+
+		} finally {
+			JDBCDataSource.closeConnection(conn);
+		}
+
+		return pk + 1;
+
+	}
+
+	// Add Method
+
+	public long add(RoleBean bean) throws ApplicationException {
+
+		Connection conn = null;
+
+		int pk = 0;
+
+		try {
+			pk = nextPk();
+			conn = JDBCDataSource.getConnection();
+			conn.setAutoCommit(false);
+			PreparedStatement pstmt = conn.prepareStatement("insert into st_role values(?,?,?,?,?,?,?)");
+
+			pstmt.setInt(1, pk);
+			pstmt.setString(2, bean.getName());
+			pstmt.setString(3, bean.getDescription());
+			pstmt.setString(4, bean.getCreatedBy());
+			pstmt.setString(5, bean.getModifiedBy());
+			pstmt.setTimestamp(6, bean.getCreatedDatetime());
+			pstmt.setTimestamp(7, bean.getModifiedDatetime());
+			pstmt.executeUpdate();
+			conn.commit();
+			pstmt.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+			try {
+				conn.rollback();
+			} catch (Exception ex) {
+				throw new ApplicationException("Exception : add rollback exception " + ex.getMessage());
+			}
+			throw new ApplicationException("Exception : Exception in add Role");
+		} finally {
+			JDBCDataSource.closeConnection(conn);
+		}
+		return pk;
+
+	}
+
+	// Delete Method
+
+	public void delete(RoleBean bean) throws ApplicationException {
+
+		Connection conn = null;
+
+		try {
+			conn = JDBCDataSource.getConnection();
+			conn.setAutoCommit(false);
+			PreparedStatement pstmt = conn.prepareStatement("delete from st_role where id = ?");
+			pstmt.setLong(1, bean.getId());
+			pstmt.executeUpdate();
+			conn.commit();
+			pstmt.close();
+
+		} catch (Exception e) {
+			try {
+				conn.rollback();
+			} catch (Exception ex) {
+				throw new ApplicationException("Exception : Delete rollback exception " + ex.getMessage());
+			}
+			throw new ApplicationException("Exception : Exception in delete Role");
+		} finally {
+			JDBCDataSource.closeConnection(conn);
+		}
+	}
+
+	// Update Method
+
+	public void update(RoleBean bean) throws ApplicationException {
+
+		Connection conn = null;
+
+		try {
+			conn = JDBCDataSource.getConnection();
+			conn.setAutoCommit(false);
+			PreparedStatement pstmt = conn.prepareStatement(
+					"update st_role set name = ?, description = ?, created_by = ?, modified_by = ?, created_datetime = ?, modified_datetime = ? where id = ?");
+			pstmt.setString(1, bean.getName());
+			pstmt.setString(2, bean.getDescription());
+			pstmt.setString(3, bean.getCreatedBy());
+			pstmt.setString(4, bean.getModifiedBy());
+			pstmt.setTimestamp(5, bean.getCreatedDatetime());
+			pstmt.setTimestamp(6, bean.getModifiedDatetime());
+			pstmt.setLong(7, bean.getId());
+			pstmt.executeUpdate();
+			conn.commit();
+			pstmt.close();
+
+		} catch (Exception e) {
+			try {
+				conn.rollback();
+			} catch (Exception ex) {
+				throw new ApplicationException("Exception : Delete rollback exception " + ex.getMessage());
+			}
+			throw new ApplicationException("Exception in updating Role ");
+		} finally {
+			JDBCDataSource.closeConnection(conn);
+		}
+	}
+
+}
---
+++ ORSProject-04/src/main/java/in/co/rays/proj4/test/TestRoleModel.java
@@ -0,0 +1,76 @@
+package in.co.rays.proj4.test;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+import in.co.rays.proj4.bean.RoleBean;
+import in.co.rays.proj4.exception.ApplicationException;
+import in.co.rays.proj4.exception.DatabaseException;
+import in.co.rays.proj4.model.RoleModel;
+
+public class TestRoleModel {
+	public static void main(String[] args) {
+
+//		RoleModel model = new RoleModel();
+//		System.out.println(model.nextPk());
+//		testAdd();
+//		testDelete();
+		testUpdate();
+
+	}
+
+	public static void testAdd() {
+
+		RoleBean bean = new RoleBean();
+		bean.setName("hr");
+		bean.setDescription("hr");
+		bean.setCreatedBy("admin");
+		bean.setModifiedBy("admin");
+		bean.setCreatedDatetime(new Timestamp(new Date().getTime()));
+		bean.setModifiedDatetime(new Timestamp(new Date().getTime()));
+
+		RoleModel model = new RoleModel();
+
+		try {
+			model.add(bean);
+			System.out.println("Role Added Successfully");
+		} catch (ApplicationException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	public static void testDelete() {
+
+		RoleBean bean = new RoleBean();
+		bean.setId(1);
+		
+		RoleModel model = new RoleModel();
+
+		try {
+			model.delete(bean);
+			System.out.println("Role deleted Successfully");
+		} catch (ApplicationException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public static void testUpdate()  {
+		
+		RoleBean bean = new RoleBean();
+		 bean.setName("hr");
+		 bean.setId(2);
+		 
+		 RoleModel model = new RoleModel();
+		 try {
+			model.update(bean);
+		} catch (ApplicationException e) {
+			
+			e.printStackTrace();
+		}
+		System.out.println("Record update successfully");
+		}
+
+}
